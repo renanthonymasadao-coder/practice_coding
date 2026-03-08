8dc2670f744b9ef82071191421b7a89d18b0d8a0
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-03-08 23:48:10 +0800

    exponent of the second power to raise it in first number

---
+++ power_of_number.py
@@ -0,0 +1,9 @@
+#Ask the user for two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#Raise the first number to the power of the second number
+result = num1 ** num2
+
+#Display the result
+print("The result is:", result)
