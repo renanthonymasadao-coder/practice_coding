6cae2fd29e04a12a91489334d415c918ddfbf204
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-03-08 23:02:55 +0800

    check equal numbers and placed a command where, if the numbers are not equal

---
+++ check_equal_numbers.py
@@ -0,0 +1,9 @@
+#Ask the user for two numbers
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+#Check if the numbers are equal
+if number1 == number2:
+    print("The numbers are equal")
+elif number1 != number2:
+    print("The numbers are not equal")
