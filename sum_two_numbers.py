637b78eed8071709802615dd7e6a5ddc33224c24
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-03-08 23:27:42 +0800

    I re upload this because I made mistake not commanding git push and uploading it manually

---
+++ sum_two_numbers.py
@@ -0,0 +1,9 @@
+#Ask the user for two numbers
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+#Add the two numbers
+sum_numbers = number1 + number2
+
+#Display the sum
+print("The sum is:", sum_numbers)
